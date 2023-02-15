efce67ba481b544a0142028b66da3bf78968792a
LorenaHolmeide <89775879+LorenaHolmeide@users.noreply.github.com>
2023-02-15 19:52:30 +0100

    Added a new branch 'components' and a folder for components

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+/*export default function Nav() {
+    return (
+        <nav>
+            <ul id="navBar" >
+                <li class="one"><a id="HTML" class="two" href="#">HTML</a></li>
+                <li class="one"><a id="CSS" class="two" href="#">CSS</a></li>
+                <li class="one"><a id="JavaScript" class="two" href="#">JavaScript</a></li>
+                <li class="one"><a id="React" class="two" href="#">React</a></li>
+                <li class="one"><a id="Sanity and headless CMS" class="two" href="#">Sanity and headless CMS</a></li>
+            </ul>
+        </nav>)
+}*/
