934ec4dd7df6a0df1da00815f8a0d2f61d3e32e5
mahojo99 <mejohan@hiof.no>
2023-02-20 11:58:15 +0100

    components stuff

--- src/components/ContentPage.js
+++ src/components/ContentPage.js
@@ -1,7 +1,22 @@
-export default function ContentPage() {
-    return(
-        <main>
+import articles from './ressurser';
+import ArticleContent from './ArticleContent';
 
+export default function ContenPage({}){
+    return (
+        <main>
+            
+            <div id="content">
+            {articles.map((item) =>{
+             return(
+             <ArticleContent  
+             name={item.title} 
+             url={item.url} 
+             cat={item.category} 
+             />   
+            )
+        } 
+        )}
+            </div>
         </main>
     )
- }
+}
