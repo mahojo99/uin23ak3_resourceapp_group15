6d9e3dbacf181b25c72e05095f8047d756092631
blustring <rchlmdn83@gmail.com>
2023-02-22 13:04:36 +0100

    Layout rerouting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import logo from './logo.svg';
 import './css/main.css';
+import CSS from './pages/CSS';
+import Html from './pages/Html';
+import React from './pages/React';
+import JavaScript from './pages/Javascipt';
+import Sanity from './pages/Sanity';
+
 //Branch Layout
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          // <Route index element={<Home />} /> 
+          <Route path="html" element={<Html />} />
+          <Route path="css" element={<CSS />} />
+          <Route path="javascript" element={<JavaScript />} />
+          <Route path="react" element={<React />} />
+          <Route path="sanity" element={<Sanity />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
+  
+  
 }
 
 export default App;
---
+++ src/components/Layout.js
@@ -0,0 +1,23 @@
+import { Link, Outlet} from "react-router-dom";
+import { BrowserRouter , Routes, Route } from "react-router-dom";
+
+
+export default function Layout(){
+    return(
+       
+        <div id="container">
+            <header>
+                <h1>Ressursarkiv</h1>
+            </header>
+            <nav>
+
+            </nav>
+            <main>
+                
+
+            </main>
+            
+
+        </div>
+    )
+}
