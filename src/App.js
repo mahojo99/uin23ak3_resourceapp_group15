efcbbfef7c19677959c3a8510ae01f8ab81b69c5
LorenaHolmeide <89775879+LorenaHolmeide@users.noreply.github.com>
2023-02-27 15:05:34 +0100

    test2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import './css/main.css';
-import CSS from './pages/CSS';
-import Html from './pages/Html';
-import React from './pages/React';
-import JavaScript from './pages/Javascipt';
-import Sanity from './pages/Sanity';
+import { Routes, Route } from 'react-router-dom';
+import ContentPage from './components/Layout';
+import CSS from './pages/CSS'
+import JAVASCRIPT from './pages/JAVASCRIPT';
+import HTML from './pages/HTML';
+import REACT from './pages/REACT';
+import SANITY from './pages/SANITY';
 
-//Branch Layout
+
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route index element={<Layout />} />
-        <Route path='/html' element={<HTML />} />
-        <Route path='/css' element={<CSS />} />
-        <Route path='/javascript' element={<JAVASCRIPT />} />
-        <Route path='/react' element={<REACT />} />
-        <Route path='/sanity' element={<SANITY />} />
-      </Routes>
+    <Routes>
+      <Route index element={<Layout />}/>
+      <Route path='/html'element={<HTML />}/>
+      <Route path='/css'element={<CSS />}/>
+      <Route path='/javascript'element={<JAVASCRIPT />}/>
+      <Route path='/react'element={<REACT />}/>
+      <Route path='/sanity'element={<SANITY />}/>
+    </Routes>
 
     </>
   );
-
-
 }
 
 export default App;
--- src/pages/Html.js
+++
@@ -1,8 +0,0 @@
-import ArticleContent from '../components/ArticleContent';
-import Resources from '../ressurser';
-
-export default function HTML() {
-    return (<div id="content">
-    {Resources.map(item => item.category === "html" ? <ArticleContent name={item.title} url={item.url} cat={item.category} /> : null)}
-    </div>)
-}
