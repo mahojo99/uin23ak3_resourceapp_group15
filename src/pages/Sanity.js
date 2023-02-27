d01c70296cda6c7bfa977366af41cd473678c9d3
LorenaHolmeide <89775879+LorenaHolmeide@users.noreply.github.com>
2023-02-27 14:53:16 +0100

    test

--- src/pages/Sanity.js
+++ src/pages/Sanity.js
@@ -1,3 +1,11 @@
-export default function Sanity(){
-    return <ContentPage />
+import ArticleContent from "../components/ArticleContent";
+import Resources from '../ressurser';
+
+export default function SANITY() {
+    return (
+        <div id="content">
+            {Resources.map(item => item.category === "headless-cms" ? <ArticleContent name={item.title} url={item.url} cat={item.category} /> : null)}
+        </div>
+            
+    )
 }
