79f2e79511fe2cdc497e3d1721ec2107fb26890a
LorenaHolmeide <89775879+LorenaHolmeide@users.noreply.github.com>
2023-02-28 10:56:58 +0100

    Layout added

---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import Nav from './Nav';
+import Header from './Header';
+export default function ContenPage({ }) {
+    //LayoutBranch
+    return (
+        <>
+            <Header />
+            <Nav />
+        </>
+
+    )
+}
