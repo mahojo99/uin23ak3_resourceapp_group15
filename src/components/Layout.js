13efa2b0a9d9001d57244a4bfd9b74fdabff6a0d
LorenaHolmeide <89775879+LorenaHolmeide@users.noreply.github.com>
2023-03-01 16:03:59 +0100

    removed unused code

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,16 @@
 import Nav from './Nav';
 import Header from './Header';
-import ArticleContent from './ArticleContent'
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
     //LayoutBranch
     return (
-    <div className='container'>
-        <Header />
-        <Nav />
-        <main>
-            {children}
-        </main>
-    </div>
-    
+        <div className='container'>
+            <Header />
+            <Nav />
+            <main>
+                {children}
+            </main>
+        </div>
+
     )
 }
